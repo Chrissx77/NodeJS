1bdfbcf440ad03d4791210192d7289c4c9414860
Chrissx77 <garozzochristian@gmail.com>
2024-06-05 19:09:54 +0200

    first commit

---
+++ persone.js
@@ -0,0 +1,34 @@
+const persone = [
+    {
+        id: '1',
+        nome: "Giovanni",
+        cognome: "Rossi",
+        età: 30
+    },
+    {
+        id: '2',
+        nome: "Maria",
+        cognome: "Bianchi",
+        età: 22
+    },
+    {
+        id: '3',
+        nome: "Luca",
+        cognome: "Verdi",
+        età: 40
+    },
+    {
+        id: '4',
+        nome: "Ludovica",
+        cognome: "Viola",
+        età: 25
+    },
+    {
+        id: '5',
+        nome: "Matteo",
+        cognome: "Neri",
+        età: 33
+    },
+];
+
+module.exports = { persone }
